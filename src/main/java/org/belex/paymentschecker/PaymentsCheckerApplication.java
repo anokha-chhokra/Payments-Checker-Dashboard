4677ea58bd8125b95067c9e933aa1fa00b5f35ff
Arbaj Ali <thewrongturn6show@gmail.com>
2026-09-02 09:36:48 +0530

    initial project setup and structure

---
+++ src/main/java/org/belex/paymentschecker/PaymentsCheckerApplication.java
@@ -0,0 +1,13 @@
+package org.belex.paymentschecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsCheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/belex/paymentschecker/PaymentsCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.belex.paymentschecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
